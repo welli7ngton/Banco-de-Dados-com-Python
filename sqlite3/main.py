ea5b0e32faab1e8feb3cf67a493528e26dac752d
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-14 12:28:12 -0300

    usando DELETE sem o WHERE

--- sqlite3/main.py
+++ sqlite3/main.py
@@ -4,21 +4,39 @@ from pathlib import Path
 ROOT_DIR = Path(__file__).parent
 DB_NAME = 'db.sqlite3'
 DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
 
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+# Deletem sem o where
+cursor.execute(
+    f'DELETE FROM {TABLE_NAME}'
+)
+connection.commit()
 
 cursor.execute(
-    'CREATE TABLE IF NOT EXISTS customers'
+    f'DELETE FROM sqlite_sequence WHERE name="{TABLE_NAME}"'
+)
+connection.commit()
+
+
+# criação de tabela
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
     '('
     'id INTEGER PRIMARY KEY AUTOINCREMENT,'
     'name TEXT,'
-    'wheigt REAL'
+    'weight REAL'
     ')'
 )
-
 connection.commit()
 
+# registrando valores nas colunas da tabela
+cursor.execute(
+    f'INSERT INTO {TABLE_NAME} (id, name, weight) '
+    'VALUES (NULL, "Wellington Almeida", 8), (NULL, "Maria Helena", 10)'
+)
+connection.commit()
 cursor.close()
 connection.close()
