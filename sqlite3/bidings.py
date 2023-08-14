b2dd05e7ab08188a015aee21a5d480fee1228bba
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-14 12:38:15 -0300

    separando valores do usuário dos comandos sql usando biding

---
+++ sqlite3/bidings.py
@@ -0,0 +1,35 @@
+import sqlite3
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+DB_NAME = 'db.sqlite3'
+DB_FILE = ROOT_DIR / DB_NAME
+TABLE_NAME = 'customers'
+
+connection = sqlite3.connect(DB_FILE)
+cursor = connection.cursor()
+
+# criação de tabela
+cursor.execute(
+    f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
+    '('
+    'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    'name TEXT,'
+    'weight REAL'
+    ')'
+)
+connection.commit()
+
+sql = (
+    f'INSERT INTO {TABLE_NAME}'
+    '(name, weight) '
+    'VALUES '
+    '(?, ?)'
+)
+
+# registrando valores nas colunas da tabela
+cursor.execute(sql, ['Richard', 14])
+connection.commit()
+print(sql)
+cursor.close()
+connection.close()
