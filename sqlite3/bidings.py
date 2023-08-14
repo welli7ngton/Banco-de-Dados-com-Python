2bf3c9af6bdef03e72cf58cd40ea36c9e00506c2
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-14 13:23:49 -0300

    adidionando DELETE

--- sqlite3/bidings.py
+++ sqlite3/bidings.py
@@ -9,6 +9,10 @@ TABLE_NAME = 'customers'
 connection = sqlite3.connect(DB_FILE)
 cursor = connection.cursor()
 
+cursor.execute(
+    f"DELETE FROM {TABLE_NAME}"
+)
+
 # criação de tabela
 cursor.execute(
     f'CREATE TABLE IF NOT EXISTS {TABLE_NAME}'
@@ -24,12 +28,18 @@ sql = (
     f'INSERT INTO {TABLE_NAME}'
     '(name, weight) '
     'VALUES '
-    '(?, ?)'
+    '(:name, :weight)'
 )
 
 # registrando valores nas colunas da tabela
-# cursor.execute(sql, ['Richard', 14])
-cursor.executemany(sql, [['Maria', 14], ['Wellington', 18]])
+# cursor.execute(sql, {'name': 'Rian', 'weight': 20})
+cursor.executemany(sql, (
+    {'name': 'Joao', 'weight': 7},
+    {'name': 'Pedro', 'weight': 3},
+    {'name': 'Carlos', 'weight': 1},
+    {'name': 'Felipe', 'weight': 11}
+    )
+)
 connection.commit()
 print(sql)
 cursor.close()
