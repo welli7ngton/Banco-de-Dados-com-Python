c872941777efd1b2ced77b72e182663d8fc3c5ed
welli7ngton <wellingtonasilva45@gmail.com>
2023-08-25 11:37:55 -0300

    testes no pymysql

--- lib_pymyql/main.py
+++ lib_pymyql/main.py
@@ -1,4 +1,5 @@
 import pymysql
+import pymysql.cursors
 import dotenv
 import os
 
@@ -9,7 +10,8 @@ connection = pymysql.connect(
     host=os.environ["MYSQL_HOST"],
     user=os.environ["MYSQL_USER"],
     passwd=os.environ["MYSQL_PASSWORD"],
-    database=os.environ["MYSQL_DATABASE"]
+    database=os.environ["MYSQL_DATABASE"],
+    cursorclass=pymysql.cursors.DictCursor,
 )
 
 with connection:
@@ -64,7 +66,64 @@ with connection:
             ("maria", 18), ("raimundo", 78)
         )
         result = cursor.executemany(sql, data1)
-        print(sql)
-        print(data1)
-        print(result)
+        # print(sql)
+        # print(data1)
+        # print(result)
     connection.commit()
+
+    with connection.cursor() as cursor:
+        # menor_id = input("digite o menor id: ")
+        # maior_id = input("digite o maior id: ")
+        menor_id = 2
+        maior_id = 4
+        sql = (
+            f'SELECT * FROM {TABLE_NAME} '
+            'WHERE id >= %s AND id <= %s'
+        )
+
+        cursor.execute(sql, (menor_id, maior_id))
+        # print(cursor.mogrify(sql, (menor_id, maior_id)))
+
+        data2 = cursor.fetchall()
+
+        # for value in data2:
+        #     print(value)
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'DELETE FROM {TABLE_NAME} '
+            'WHERE id = %s'
+        )
+
+        # cursor.execute(sql, (1))
+        # cursor.execute(sql, (2))
+        # cursor.execute(sql, (3))
+        # connection.commit()
+
+        # cursor.execute(f'SELECT * FROM {TABLE_NAME} ')
+
+        # for value in cursor.fetchall():
+        #     print(value)
+
+    with connection.cursor() as cursor:
+        sql = (
+            f'UPDATE {TABLE_NAME} '
+            'SET name=%s, age=%s '
+            'WHERE id=%s'
+        )
+
+        # cursor.execute(sql, ("teste", 99, 1))
+        # connection.commit()
+        # cursor.execute(sql, ("RAIMUNDO", 75, 6))
+        # connection.commit()
+
+        # cursor.execute(f'SELECT * FROM {TABLE_NAME} ')
+
+        # for value in cursor.fetchall():
+        #     print(value)
+
+    with connection.cursor() as cursor:
+        cursor.execute(f'SELECT * FROM {TABLE_NAME} ')
+
+        for value in cursor.fetchall():
+            print(value)
